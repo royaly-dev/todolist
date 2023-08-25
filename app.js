5a5dff2f4669a256a3d8df71649210f64611b8e6
royaly <81520338+royalygames@users.noreply.github.com>
2023-08-25 10:55:28 +0200

    mise à jour de la todo, préparation pour l'ajout d'une api

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const aFaire = document.querySelector('#aFaire')
 const tous = document.querySelector('#tous')
 const fait = document.querySelector('#fait')
 const newTodoAdd = document.querySelector('form')
-
+const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+</svg>`
 /*
 system de suprétion
 */
@@ -43,9 +46,9 @@ function addNewTodo(title) {
     newTodo = document.createElement('div')
     newTodo.className = 'todo'
     newTodo.innerHTML = `
-        <input type="checkbox">
+        <input class="check-box" type="checkbox">
         <h3>${title}</h3>
-        <button>delete</button>
+        <button>${svg}</button>
     `
     loader.append(newTodo)
     deleteTodo()
@@ -113,9 +116,9 @@ function verifcheck(Todo, option) {
         const verif = document.createElement('div')
         verif.className = 'todo'
         verif.innerHTML = `
-        <input type="checkbox" ${input}>
+        <input class="check-box" type="checkbox" ${input}>
         <h3>${Todo.title}</h3>
-        <button>delete</button>`
+        <button>${svg}</button>`
         return verif
     } return ''
 
@@ -132,9 +135,9 @@ function createNewTodo(Todo) {
     const newTodo = document.createElement('div')
     newTodo.className = 'todo'
     newTodo.innerHTML = `
-        <input type="checkbox" ${input}>
+        <input class="check-box" type="checkbox" ${input}>
         <h3>${Todo.title}</h3>
-        <button>delete</button>
+        <button>${svg}</button>
     `
 
     return newTodo
